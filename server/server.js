5ca651fcc8187efd6579204a0b4419f3e1afcbb7
Adam Boerhave <arboerhave@gmail.com>
2020-10-11 21:03:17 -0500

    comments in index.html and server.js file

--- server/server.js
+++ server/server.js
@@ -3,8 +3,7 @@ const express = require('express');
 // require body-parser to parse through data
 const bodyParser = require('body-parser');
 
-// creates instance of express by calling the function
-// returned above - gives us an object
+// creates instance of express by calling the function returned above - gives us an object
 const app = express();
 const PORT = 5000;
 
@@ -12,10 +11,12 @@ app.use(express.static('server/public'));
 
 app.use(bodyParser.urlencoded({extended: true}));
 
+// array to store calculations after they are posted for first calculator
 let calculationsArray = [];
+// array to store calculations after they are posted for second calculator
 let buttonCalculationsArray = [];
 
-// get data coming from the client.js file
+// This section is the POST request for the data coming from the client.js file for the first calculator
 app.post('/operation', (req, res) => {
     // req.body is data that comes from client.js
     console.log('hello from post request', req.body);
@@ -28,29 +29,25 @@ app.post('/operation', (req, res) => {
     console.log('newValue', newValue);
     let result = mathOperation(Number(newValue.value1), newValue.operator, Number(newValue.value2));
     newValue.result = result;
-
     console.log('result', result);
     console.log('new object', newValue);
     calculationsArray.push(newValue);
     console.log('updated array', calculationsArray);
     
-    
     res.sendStatus(200);
-})
+})  // end post /operation
 
-// need get request so client.js can GET data of previous calculations from
-// the server
+// This is the GET request section to send the calculationsArray to the client.js file for the first calculator
 app.get('/previousResults', (req, res) => {
     console.log('operations get request');
     res.send(calculationsArray);
-    
-});
+}); // end GET /previousResults
 
-
+// This function does the math for the first calculator after taking values and an operator from the post section
 function mathOperation(value1, operator, value2) {
     console.log(value1, operator, value2);
-    
-    switch(operator) {
+    // switch statement depending on operator entered
+    switch (operator) {
         case "+":
             console.log('addition');
             return value1 + value2;            
@@ -66,6 +63,8 @@ function mathOperation(value1, operator, value2) {
     }
 } // end mathOperation function
 
+//////////////////////////////////////////////////////// SECTION FOR CALCULATOR WITH BUTTON INTERFACE //////////////////////////////////////////////
+
 // POST for expresions from calculator with interface
 app.post('/buttonCalculation', (req, res) => {
     console.log('hello from /submitCalculation post', req.body);
@@ -76,10 +75,6 @@ app.post('/buttonCalculation', (req, res) => {
     console.log('arrayOfExpression', arrayOfExpression);
     let arrayToExamine = arrayOfExpression.slice(0,arrayOfExpression.length);
     console.log('arrayToExamine', arrayToExamine);
-    //let value1 = Number(arrayOfExpression[0]);
-    //let operator = arrayOfExpression[1];
-    //let value2 = Number(arrayOfExpression[2]);
-    //let result = buttonMathOperation(value1, operator, value2);
     let result = doTheMath(arrayToExamine);
     arrayOfExpression.push('=');
     arrayOfExpression.push(result);
@@ -88,9 +83,9 @@ app.post('/buttonCalculation', (req, res) => {
     console.log('buttonsCalculationsArray', buttonCalculationsArray);
      
     res.sendStatus(200);    
-})
+})  // end post /buttonCalculation
 
-// want to take array and find first three elements to calculate
+// want to take array and find first three elements to calculate based on math order of operations
 function doTheMath(array) {
     console.log('array in doTheMath', array);
     let numberOfIterations = Math.floor(array.length/2);
@@ -114,9 +109,9 @@ function doTheMath(array) {
                 result = buttonMathOperation(value1, operator, value2);
                 array.splice(multiplyIndex-1,0,result);
             }
+            // if divide comes before multiplication
             else if(divideIndex < multiplyIndex) {
                 console.log('option2');
-                
                 arrayToCheck = array.splice(divideIndex-1, 3);
                 console.log('arrayToCheck', arrayToCheck);
                 let value1 = arrayToCheck[0];
@@ -126,6 +121,7 @@ function doTheMath(array) {
                 array.splice(divideIndex-1,0,result);
             }
         }
+        // if only multiply
         else if (multiplyIndex > 0) {
             console.log('option 3');
             console.log('multiplyIndex', multiplyIndex);
@@ -139,6 +135,7 @@ function doTheMath(array) {
             array.splice(multiplyIndex-1,0,result);
             console.log('array', array);
         }
+        // if only divide
         else if(divideIndex > 0) {
             console.log('option4');
             arrayToCheck = array.splice(divideIndex-1, 3);
@@ -151,6 +148,7 @@ function doTheMath(array) {
             array.splice(divideIndex-1,0,result);
             console.log('array', array);
         }
+        // should only be addition and subtraction left here
         else {
             console.log('option5');
             arrayToCheck = array.splice(0, 3);
@@ -164,18 +162,14 @@ function doTheMath(array) {
             console.log('array', array);
         }
     }
-    return array[0];
-}
+    return array[0];    // returns the last value in the array which should only be the result left
+}   // end doTheMath fn
 
-// function checkForLowestMorD(array){
-//     let multiplyIndex 
-
-// }
-
-// this one is just different because it still has operators as X and ÷
+// This function does the math for the calculator with the button interface.
+// It is only different from the other one because it still has operators as X and ÷
 function buttonMathOperation(value1, operator, value2) {
     console.log("buttonMathOperation", value1, operator, value2);
-    
+    // switch statement for doing the math calculation depending on operator
     switch(operator) {
         case "+":
             console.log('addition');
@@ -190,25 +184,24 @@ function buttonMathOperation(value1, operator, value2) {
             console.log('division');
             return value1 / value2;
     }
-} // end buttonMathOperation function
+} // end buttonMathOperation fn
 
-// want to do get request to get previous calculations to client.js
+// section for get request to get previous calculations to client.js
 app.get('/previousButtonResults', (req, res) => {
     console.log('button get request');
     res.send(buttonCalculationsArray);
-})
+})  // end get /previousButtonResults
 
-// DELETE request
+// DELETE request to delete the previous calculations from the second calculator
 app.delete('/delete', (req, res) => {
     console.log('delete request');
     buttonCalculationsArray.splice(0, buttonCalculationsArray.length);
     console.log('buttonCalculationsArray', buttonCalculationsArray);
     res.sendStatus(200);
-    
-})
+})  // end delete request
 
 // start up server
 app.listen(PORT, () => {
     console.log('Up and running on port: ', PORT);
     
-});
+}); // end listem
